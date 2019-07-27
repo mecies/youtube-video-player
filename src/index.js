dbec4d6b4092c5f20417f0b04958071f1fac372a
unknown <maciekhnat@gmail.com>
2019-07-27 14:59:53 +0200

    removed some autogenerated files and added app component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return <div>I'm an app component</div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
